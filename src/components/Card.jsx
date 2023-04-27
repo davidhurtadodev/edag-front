a164296bbf209b3ed4d7f2060d6ea7103b55e070
ElionorVendrell <elivendrell@gmail.com>
2023-04-27 14:32:43 +0200

    card changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,21 @@
+import Link from "next/link";
+
 export function Card({ squad, url_imagen }) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        {/* <img
-          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
+    <>
+      {/*       <Link key={squad.id} href={`/RecipeDetail/${squad.id}`}> */}
+      <div className='card w-60 m-6 bg-base-100 shadow-xl flex items-center'>
+        <figure className='w-50 rounded-full mt-6 '>
+          {/*  <img
+          src='url_imagen'
+          alt=`${squad}`
         /> */}
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{squad}</h2>
+        </figure>
+        <div className='card-body'>
+          <h1 className='card-title'>{squad}</h1>
+        </div>
       </div>
-    </div>
+      {/*       </Link> */}
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,19 @@
-import { Inter } from 'next/font/google';
-import { Card } from '@/components/Card';
-import { Grid } from '@/components/Grid';
+import { Inter } from "next/font/google";
+import { Card } from "@/components/Card";
+import { Grid } from "@/components/Grid";
 
-const inter = Inter({ subsets: ['latin'] });
-
+const inter = Inter({ subsets: ["latin"] });
 
 import { Layout } from "@/components/Layout";
 import { TeamCard } from "@/components/TeamCard";
 
-
 export default function Home() {
   return (
     <div>
-
       <Layout>
+        <Card />
         <TeamCard />
       </Layout>
-
     </div>
   );
 }
