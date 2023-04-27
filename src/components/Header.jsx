a87676e38431b3d13d050943ab2fbf52dd2921e1
ElionorVendrell <elivendrell@gmail.com>
2023-04-27 12:07:34 +0200

    header & footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className='navbar navbar-center bg-base-100'>
+        <div className='flex-1'>
+          <a className='btn btn-ghost normal-case text-xl'>daisyUI</a>
+        </div>
+      </div>
+    </div>
+  );
+};
