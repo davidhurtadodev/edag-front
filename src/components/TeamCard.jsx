2f5ffa9ff95ad237f48a9fa9ba980de156fdbcc5
ElionorVendrell <elivendrell@gmail.com>
2023-04-27 13:37:20 +0200

    context squads

---
+++ src/components/TeamCard.jsx
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { TeamsContext } from "../context/SquadContext";
+
+export const TeamCard = () => {
+  const teams = useContext(TeamsContext);
+
+  console.log(teams, "teams");
+  return <div>hola</div>;
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 import { Layout } from "@/components/Layout";
+import { TeamCard } from "@/components/TeamCard";
 
 export default function Home() {
   return (
     <div>
       <Layout>
-        
+        <TeamCard />
       </Layout>
     </div>
   );
